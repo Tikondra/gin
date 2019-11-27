e29ba8ae08c30e5131d5b9ee8c0c97ab874d59e4
Tikondra <52007300+Tikondra@users.noreply.github.com>
2019-11-27 14:08:25 +0400

    save

---
+++ js/script2.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(".page-nav__btn").click(function(){
+        $("#panel").slideToggle("slow");
+        $(this).toggleClass("active");
+    });
+});
+
+$(function() {
+ $(".owl-carousel").owlCarousel({
+ 	items: 3,
+ 	nav: true,
+ 	dots: false,
+ 	autoplay: false
+ });
+});
